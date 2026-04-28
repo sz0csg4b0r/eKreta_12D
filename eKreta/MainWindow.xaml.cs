bc0993f6ae55f61cbdb3495fb42cb492c134b528
sz0csg4b0r <szocsgabor@turr.hu>
2026-04-28 09:58:35 +0200

    .

--- eKreta/MainWindow.xaml.cs
+++ eKreta/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace eKreta
 {
@@ -25,7 +17,6 @@ namespace eKreta
         {
 
         }
-   
 
         private void tanarMenu_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
