6f22f9ff73c261f25e79280b89c848b1184bb5f2
sz0csg4b0r <szocsgabor@turr.hu>
2026-05-27 12:38:49 +0200

    feat:userLogin

---
+++ eKreta/Views/UserLogin.xaml.cs
@@ -0,0 +1,66 @@
+﻿using eKreta.Models;
+using eKreta.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace eKreta.Views
+{
+    /// <summary>
+    /// Interaction logic for UserLogin.xaml
+    /// </summary>
+    public partial class UserLogin : Window
+    {
+        public UserLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string userName = loginUserText.Text;
+            string passwordHash = PasswordHelper.HashPassword(loginPasswordText.Password);
+
+            if (!string.IsNullOrEmpty(loginUserText.Text) || !string.IsNullOrEmpty(loginPasswordText.Password))
+            {
+                using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
+                {
+                    var user = connection.Table<Felhasznalo>().FirstOrDefault(u => u.FelhasznaloNev == userName);
+
+                    //Ha van ilyen felhasználó
+                    if (user != null)
+                    {
+                        // jelszóellenőrzés
+                        if (user.Jelszo == passwordHash)
+                        {
+                            // Sikeres 
+
+
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Belépés megtagadva! ");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Belépés megtagadva! ");
+                    }
+                }
+
+            }
+
+        }
+    }
+}
